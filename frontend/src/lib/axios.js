ace5b256f98e4c8d198a08f80e970ffa88e6258a
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-08-26 11:27:02 +0530

    adding favicon

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://be-chitchat.supxdevs.com/api",
     withCredentials: true
 })
