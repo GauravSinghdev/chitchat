edd8d7664cd7ef257ee508f28e78c1d92514b574
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-08-26 11:00:25 +0530

    added node index.js in package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,14 +10,13 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 const port = process.env.PORT;
 
-// Increase body size limit
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-
+// Increase body size limit
 
 
 app.get("/", (_, res) => {
